789d97a495f054105e61491f85cfb54d19d569ae
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-08-16 23:15:35 -0500

    completed phase-1-docmcontedloaded

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // This code will run when the DOM is fully loaded and ready to be manipulated
+    const textParagraph = document.getElementById("text").textContent = "This is really cool!";
+    //textParagraph.textContent = "This is really cool!";
+  });
+  
+
+  
